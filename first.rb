1fd3da2c689fb261ba813e17be9094bcc7cfcd3c
EC2 Default User <ec2-user@ip-172-31-9-112.eu-west-1.compute.internal>
2018-06-21 07:17:44 +0000

    My first Chef Cookbookgit status

---
+++ first.rb
@@ -0,0 +1,17 @@
+# Creation d'un repertoire
+directory 'C:FGL' do
+  action :create
+  recursive true
+  rights :full_control, "Admin", :applies_to_children => true
+end
+
+# creation d'un fichier text avec contenu
+File ‘c:\FGL\myfile.txt’ do
+ Content ‘This is a test file”
+ action :create
+end
+
+# Activation d'une feature
+windows_feature "TelnetClient" do
+  action :install
+end
