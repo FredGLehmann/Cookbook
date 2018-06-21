d8c3db70a7c147d7af58da9692bccb86891a81a9
EC2 Default User <ec2-user@ip-172-31-9-112.eu-west-1.compute.internal>
2018-06-21 08:21:32 +0000

    regalge quote

--- test/recipes/first.rb
+++ test/recipes/first.rb
@@ -1,13 +1,13 @@
 # Creation d'un repertoire
-directory 'C:FGL' do
+directory 'C:\FGL' do
   action :create
   recursive true
   rights :full_control, "Admin", :applies_to_children => true
 end
 
 # creation d'un fichier text avec contenu
-File ‘c:\FGL\myfile.txt’ do
- Content ‘This is a test file”
+file 'c:\FGL\myfile.txt' do
+ Content 'This is a test file'
  action :create
 end
 
